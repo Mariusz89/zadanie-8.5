f8cbb8910a3f30129561f0322e0645ef29eebcf2
Mariusz Kosiór <mariusz.kkk@poczta.fm>
2017-09-26 21:21:35 +0200

    first commit

--- scripts.js
+++ scripts.js
@@ -4,9 +4,9 @@ var numberList = document.getElementsByTagName("li");
 
 
 add.addEventListener('click', function() {
-var element = document.createElement('li');
-element.innerHTML = 'item ' + numberList.length;
-list.appendChild(element);
+	var element = document.createElement('li');
+	element.innerHTML = 'item ' + numberList.length;
+	list.appendChild(element);
 });
 
 
